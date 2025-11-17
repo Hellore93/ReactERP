d8352f8ab689931b718ff7a34d078c4df7e7e681
Sztorc <rsztorc@luxoft.com>
2025-11-17 12:33:15 +0100

    Fix route

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { DataStoreProvider } from "./store/DataStore.jsx";
 import App from "./App.jsx";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ReactERP">
+    <HashRouter>
       <DataStoreProvider>
         <App />
       </DataStoreProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
